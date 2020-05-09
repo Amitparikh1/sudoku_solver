c67a77bc5f398b01074c118f23ed8a394d46e8c4
Amit Parikh <amitparikh41@gmail.com>
2020-05-09 13:18:13 -0400

    print_board and find_empty functions added

--- solver.py
+++ solver.py
@@ -10,3 +10,25 @@ board = [
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]
 ]
+
+## Function to pick an empty square
+## Function to loop through numbers
+## Function to find if the number is valid
+## Function to print out board 
+def print_board(board):
+    for i in range(len(board)):
+        ## Print horizontal lines after every 3rd row
+        if i % 3 == 0 and i != 0 :
+            print("------------------------")
+        ## Iterate through each value in a row 
+        for j in range(len(board[0])):
+            ## print vertical lines after every 3rd column
+            if j % 3 == 0 and j!=0:
+                print(" | ", end = "") ##print but don't go to the next line
+            ## if it is the last number in the row allow to print on the next line
+            if j == 8:
+                print(board[i][j])
+            else: 
+                print(str(board[i][j])+" ",end = "")
+print_board(board)
+
