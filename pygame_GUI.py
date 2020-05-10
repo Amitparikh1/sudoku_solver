8a04537cbd62065a780cb71b203ff01b53d82cc5
Amit Parikh <amitparikh41@gmail.com>
2020-05-10 12:03:41 -0400

    initialized pygame, created screen and game loop

--- pygame_GUI.py
+++ pygame_GUI.py
@@ -1,1 +1,15 @@
+## Import 
 import pygame
+
+## Initialize pygame
+pygame.init()
+
+## Create the screen
+screen = pygame.display.set_mode((800,600))
+
+## Game Loop 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
