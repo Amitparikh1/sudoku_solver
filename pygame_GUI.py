060d412410dc7a860cff3399076794eb8b654bfd
Amit Parikh <amitparikh41@gmail.com>
2020-05-11 15:44:53 -0400

    added name and icon image to pygame

--- pygame_GUI.py
+++ pygame_GUI.py
@@ -1,15 +1,29 @@
 ## Import 
 import pygame
 
-## Initialize pygame
-pygame.init()
+def main():
+    ## Initialize pygame
+    pygame.init()
 
-## Create the screen
-screen = pygame.display.set_mode((800,600))
+    ## Create the screen
+    screen = pygame.display.set_mode((800,600))
 
-## Game Loop 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    ## Title and Icon
+    pygame.display.set_caption("Sudoku")
+    icon = pygame.image.load("images/sudoku_image.png")
+    pygame.display.set_icon(icon)
+
+    ## Game Loop 
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        ## Background color
+        screen.fill((160, 202, 217))
+
+        ## Update display
+        pygame.display.update()
+
+## Call main function
+main()
